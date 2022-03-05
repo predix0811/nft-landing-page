02318b273a5163507a70e6e4dc2fc809527be202
Luka Predalic <luka.predalic0811@gmail.com>
2022-03-05 19:40:39 +0100

    mm

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // METAMASK CONNECTION
 const TIMEOUT = 1000;
-const COLLECTION_NAME = 'CodeCats';
+const COLLECTION_NAME = 'FutureAir Froggies';
 let editions = [];
 let dots = 1;
 
@@ -80,7 +80,7 @@ function updateStatusText(isOwner, checking) {
     if(isOwner) {
       statusText.innerText = `You do own ${COLLECTION_NAME}!! 😻 Let's see how many${renderDots(dots)}`;
     } else {
-      statusText.innerText = `Checking to see if you own any ${COLLECTION_NAME} 😻${renderDots(dots)}`;
+      statusText.innerText = `MetaMask Connected${renderDots(dots)}`;
     }
   } else {
     if(isOwner) {
